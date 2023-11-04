50294e5447128e009adb0d510e55330f4b874a25
Lyna028 <Kettanilyna@gmail.com>
2023-11-04 15:27:52 -0400

    adding filter

--- filter.py
+++ filter.py
@@ -1,3 +1,17 @@
+import os
+import numpy as np
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+import matplotlib
+from tqdm import tqdm
+#import sklearn as sk
 
+path = os.getcwd()
+volumeDefaultData = pd.read_csv(os.path.join(path, 'series/volume.csv'))
+openDefaultData = pd.read_csv(os.path.join(path, 'series/open.csv'))
+highDefaultData = pd.read_csv(os.path.join(path, 'series/high.csv'))
+closeDefaultData = pd.read_csv(os.path.join(path, 'series/close.csv'))
+lowDefaultData = pd.read_csv(os.path.join(path, 'series/low.csv'))
 
-if __name__ == '__main__':
+def filterYears() :
---
+++ main.py
@@ -0,0 +1,4 @@
+from filter import filterYears
+
+if __name__ == '__main__':
+    filterYears()
